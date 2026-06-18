7eca74ddadaec9c4aed082accfa0a440fb7835bb
CorranHornet <texhex14@outlook.com>
2026-06-18 11:41:10 +0200

    Refactor: Improve project structure and namespaces

--- ApplicationLayer/Interfaces/IGameRepository.cs
+++ ApplicationLayer/Interfaces/IGameRepository.cs
@@ -1,10 +1,11 @@
-﻿using DomainLayer.Models;
-
-public interface IGameRepository
+﻿namespace DomainLayer.Models
 {
-    Task<List<Game>> GetAllAsync(int? tournamentId, string? search);
-    Task<Game?> GetByIdAsync(int id);
-    Task AddAsync(Game game);
-    Task DeleteAsync(Game game);
-    Task SaveAsync();
+    public interface IGameRepository
+    {
+        Task<List<Game>> GetAllAsync(int? tournamentId, string? search);
+        Task<Game?> GetByIdAsync(int id);
+        Task AddAsync(Game game);
+        Task DeleteAsync(Game game);
+        Task SaveAsync();
+    }
 }
